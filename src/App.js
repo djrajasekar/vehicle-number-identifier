f432253ad4df025bc311a3e209f57914a831849b
djraj <dj.rajasekar@gmail.com>
2026-02-20 17:08:02 -0500

    Add necessary imports for AWS S3 and React hooks in App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,133 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import { S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
+import { MdOnlinePrediction } from "react-icons/md";
 import './App.css';
 
+const bucketName = "vehicle-identifier-bucket"
+const creds = {
+  accessKeyId: "AKIAY3CRXOHOJYNO37CK",
+  secretAccessKey: "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",
+};
+
+const client = new S3Client({
+  region: "us-east-1",
+  signatureVersion: 'v4',
+  credentials: creds
+});
+
 function App() {
+  const [image, setImage] = useState(null);
+  const [message, setMessage] = useState("");
+  const [ws, setWs] = useState(undefined);
+  const [numberPlate, setNumberPlate] = useState("");
+  const [socketStatusColour, setSocketStatusColour] = useState("grey")
+
+  useEffect(() => {
+    if (!ws) {
+      console.log('........Connecting to server...........')
+      const webSocket = new WebSocket("wss://mwtqeze40m.execute-api.us-east-1.amazonaws.com/dev-vehicle/");
+      setWs(webSocket)
+    } else {
+      setSocketStatusColour("grey")
+    }
+  }, []);
+
+
+  if (ws) {
+    ws.onopen = (event) => {
+      console.log('Connection established', event)
+      setSocketStatusColour("green")
+    };
+    ws.onmessage = function (event) {
+      console.log(`event`, event)
+      try {
+        const data = JSON.parse(event.data);
+        console.log('Number:', data.message)
+        setMessage("")
+        setNumberPlate(`Number plate: ${data.message}`);
+
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }
+
+  const processImage = async (event) => {
+    setNumberPlate(`Number plate: .....`);
+    setImage(event.target.files[0])
+
+    const uploadResult = await uploadFileToS3(event.target.files[0])
+    if (uploadResult) {
+      const imageName = event.target.files[0].name;
+      await sendMessage(imageName);
+    }
+  }
+
+  const uploadFileToS3 = async (image) => {
+    setMessage("Uploading......")
+    const target = { Bucket: bucketName, Key: image.name, Body: image };
+    try {
+      const parallelUploads3 = new Upload({
+        client: client,
+        // tags: [{ Key: "connection_id", Value: "123"}], // optional tags
+        queueSize: 4, // optional concurrency configuration
+        leavePartsOnError: false, // optional manually handle dropped parts
+        params: target,
+      });
+
+      parallelUploads3.on("httpUploadProgress", (progress) => {
+        console.log("progress:", progress);
+      });
+
+      var result = await parallelUploads3.done();
+
+      setMessage("Upload is done")
+
+      // console.log('Result:', result);
+      return true;
+
+    } catch (e) {
+      console.log(e);
+      setMessage("Upload is failed");
+    }
+    return false;
+  }
+
+  const sendMessage = async (imageName) => {
+    try {
+      if (ws) {
+        setMessage(`Sending image ${imageName} info....`)
+
+        const result = await ws.send(JSON.stringify({
+          "action": "sendVehicleInfo",
+          "message": { "bucket": bucketName, "key": imageName }
+        }));
+
+        console.log('result', result)
+        setMessage("Processing image....")
+      }
+    }
+    catch (err) {
+      console.log("error", err)
+    }
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Vehicle Number Plate Recognition System</h3>
+        <div id="web-socket-status"> <MdOnlinePrediction color={socketStatusColour} size={100} /></div>
+        {
+          image === null ? <div></div> : <img src={URL.createObjectURL(image)} style={{ height: 400, width: 400 }} />
+        }
+
+        <br></br>
+        <input type="file" onChange={processImage} />
+        <hr />
+        <label id="message">{message}</label>
+        <label id="numberPlate">{numberPlate}</label>
       </header>
     </div>
   );
